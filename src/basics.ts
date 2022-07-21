77a1612cda7dec4a45ba5e78a44ec15082691b4f
Eugene Roy <box.rz.box@gmail.com>
2022-07-21 16:54:06 +0300

    funcs and generics basics

--- src/basics.ts
+++ src/basics.ts
@@ -55,3 +55,43 @@ type Student = {
 let student: Student;
 let students: Student[];
 //----------------------------------------------------------------
+//  Functions and types
+//  No need to explicitly defined types, TS do it automaticly.
+//  Except cases when you needed ;)
+function add(a: number, b: number): number | string {
+    return a + b;
+}
+
+//  With no return have VOID type value. 
+function printOutput(value: any) {
+    console.log(value)
+}
+//----------------------------------------------------------------
+//  Generics
+//  Helps write flexible, but type sensitive funcs
+
+//  Base variant
+// function insertAtBeginning(array: any, value: any) {
+//     const newArray = [value, ...array];
+//     return newArray;
+// }
+// const demoArray = [1, 2, 3, 4, 5];
+// const updatedDemo = insertAtBeginning(demoArray, 0); // [0,1,2,3,4,5]
+
+// updatedDemo[0].split('') // Runtime error, have type any, TS will not react
+
+//  With generic usage
+function insertAtBeginning<T>(array: T[], value: T) {
+    const newArray = [value, ...array];
+    return newArray;
+}
+const demoArray = [1, 2, 3, 4, 5];
+const updatedDemo = insertAtBeginning(demoArray, 0); // [0,1,2,3,4,5]
+
+// updatedDemo[0].split('') // TS sees error
+
+//  Can not just use the angle brackets to define a generic type 
+//  but also to USE a generic type and explicitly set the placeholder type 
+//  that should be used - sometimes this is required if TypeScript is not able to 
+//  infer the(correct) type.
+const stringArray = insertAtBeginning<string>(['a', 'b', 'c'], 'd');
