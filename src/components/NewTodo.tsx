9c2b58dad40e429846a59ca6706f3e0ae6946202
Eugene Roy <box.rz.box@gmail.com>
2022-07-25 16:36:10 +0300

    added submit handler in todo, using ref

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const NewTodo = () => {
+    const todoInputRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const text = todoInputRef.current?.value;
+
+        //  If value definitly will be not nullish : 
+        // const text = todoInputRef.current!.value;
+
+        if (text?.trim().length === 0) {
+            // Throw Error
+            return
+        }
+    }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <label htmlFor="todo">Todo text:</label>
-            <input type="text" name="todo" id="todo" />
+            <input type="text" name="todo" id="todo" ref={todoInputRef}/>
             <button>Add Todo</button>
         </form>
     );
